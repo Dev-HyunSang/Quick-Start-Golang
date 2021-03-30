51760445ddb342738f5c7129b276f8a2bf6bcc1f
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-03-30 22:24:07 +0900

    [UPDATE🚀]  Functions part Two

--- chapter-01/03.go
+++ chapter-01/03.go
@@ -13,6 +13,13 @@ func lenAndUpper(name string) (int, string) {
 	return len(name), strings.ToUpper(name)
 }
 
+func lenAndUpperTwo(name string) (lenght int, uppercase string)  {
+	defer fmt.Println("I'm done!")
+	lenght = len(name)
+	uppercase = strings.ToUpper(name)
+	return
+}
+
 func repeatMe(words ...string) {
 	fmt.Println(words)
 }
@@ -21,6 +28,8 @@ func main() {
 	fmt.Println(multiply(2, 3))
 	// _는 무시되는 변수임. => 무시된 상태로 컴파일
 	totalLenght, upperName := lenAndUpper("HyunSang")
+
 	fmt.Println(totalLenght, upperName)
 	repeatMe("HyunSang", "Rin", "Nico")
+	fmt.Println(lenAndUpperTwo("HyunSang"))
 }
